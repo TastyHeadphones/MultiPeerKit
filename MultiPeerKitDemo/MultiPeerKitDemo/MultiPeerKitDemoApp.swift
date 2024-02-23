517736f5bd26e2e10b70f8e8acedec6ac5fe72c9
Young <creel-diurnal0c@icloud.com>
2024-02-23 10:07:32 +0800

    [Init] Stub package and demo

---
+++ MultiPeerKitDemo/MultiPeerKitDemo/MultiPeerKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiPeerKitDemoApp.swift
+//  MultiPeerKitDemo
+//
+//  Created by Young Geo on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiPeerKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
